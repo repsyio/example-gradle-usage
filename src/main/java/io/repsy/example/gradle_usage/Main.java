ad6ca09a7d20b543887deab39355a5f1ce9dee63
firatkucuk <firat@fria.io>
2020-06-22 23:29:36 +0200

    initial commit

---
+++ src/main/java/io/repsy/example/gradle_usage/Main.java
@@ -0,0 +1,10 @@
+package io.repsy.example.gradle_usage;
+
+import io.repsy.example.gradle_deploy.Hello;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Hello.sayHello("World"));
+    }
+}
